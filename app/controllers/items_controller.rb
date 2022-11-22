413289b47b9a4e9ae2253f7d139bdab1bc04f4cd
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-11-22 13:48:54 +0000

    Created new.html for items and created the action new/create in the items controller.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,28 @@ class ItemsController < ApplicationController
   end
 
   def show
-    authorize @item
   end
 
   def new
+    @item = Item.new
+    authorize @item
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    authorize @item
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :value, :current_situation, :category, :location)
   end
 end
